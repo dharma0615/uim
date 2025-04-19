fa3560439af31d36c0fc7ea3551b3c6f5dd4b423
dharma <dharma.marada@gmail.com>
2025-04-19 07:33:03 +0530

    Login file page factory added

--- src/main/java/com/hcl/uimpages/LoginPage.java
+++ src/main/java/com/hcl/uimpages/LoginPage.java
@@ -1,5 +1,39 @@
 package com.hcl.uimpages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class LoginPage {
+	
+	WebDriver driver;
+	
+	@FindBy(name="email")
+	private WebElement emailAddress;
+	
+	@FindBy(xpath="//input[@type='password']")
+	private WebElement password;
+	
+	@FindBy(xpath="//button[contains(text(),'Login')]")
+	private WebElement login;
+	
+	public LoginPage(WebDriver driver) {
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+		
+	}
+	
+	public void enterUserEmail(String email) {
+		emailAddress.sendKeys(email);
+	}
+	
+	public void enterUserPassword(String userpassword) {
+		password.sendKeys(userpassword);
+	}
+	
+	public void clickLogin() {
+		login.click();
+	}
 
 }
